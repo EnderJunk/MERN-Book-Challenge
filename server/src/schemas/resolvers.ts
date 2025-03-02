36b9cc9f19edbce16cb738b6e934f0607c496048
EnderJunk <christian.walters1996@gmail.com>
2025-03-01 19:35:34 -0700

    built out schemas directory

---
+++ server/src/schemas/resolvers.ts
@@ -0,0 +1,54 @@
+import User from '../models/User.js';
+import { signToken } from '../services/auth.js';
+
+export const resolvers = {
+  Query: {
+    me: async (_parent, _args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new Error('You need to be logged in!');
+    },
+  },
+  Mutation: {
+    login: async (_parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new Error("Can't find this user");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new Error('Wrong password!');
+      }
+      const token = signToken(user.username, user.email, user._id);
+      return { token, user };
+    },
+    addUser: async (_parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user.username, user.email, user._id);
+      return { token, user };
+    },
+    saveBook: async (_parent, { bookData }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: bookData } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+      throw new Error('You need to be logged in!');
+    },
+    removeBook: async (_parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new Error('You need to be logged in!');
+    },
+  },
+};
