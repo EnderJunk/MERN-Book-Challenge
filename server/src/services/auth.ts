d87cf9d586a1b9c3ab6382deb0ef9102f017d0ca
EnderJunk <christian.walters1996@gmail.com>
2025-03-01 19:16:19 -0700

    updated auth.ts in the server side

--- server/src/services/auth.ts
+++ server/src/services/auth.ts
@@ -10,24 +10,13 @@ interface JwtPayload {
   email: string,
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
-
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
-
-    const secretKey = process.env.JWT_SECRET_KEY || '';
-
-    jwt.verify(token, secretKey, (err, user) => {
-      if (err) {
-        return res.sendStatus(403); // Forbidden
-      }
-
-      req.user = user as JwtPayload;
-      return next();
-    });
-  } else {
-    res.sendStatus(401); // Unauthorized
+export const authenticateToken = (token: string) => {
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+  try {
+    const user = jwt.verify(token, secretKey) as JwtPayload;
+    return user;
+  } catch (err) {
+    throw new Error('Invalid token');
   }
 };
 
