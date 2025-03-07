5d58a1c55fffe720caefb4ac1d821912c5c9916c
EnderJunk <christian.walters1996@gmail.com>
2025-03-07 00:14:09 -0700

    define __dir

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,12 @@ import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
 import { authenticateToken } from './utils/auth.js';
 
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
